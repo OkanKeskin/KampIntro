509803b7911df13fd7f4586e9d90327fab55b154
Okan Keskin <okannkeskn@gmail.com>
2021-03-20 15:07:19 +0300

    20.03 Ders sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product  //Entity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }  //Kategoriler Id ile belirtilir ve 2.sıraya yazılır.
+        public string ProductName { get; set; } //Ürün adı
+        public double UnitPrice { get; set; }  //ürün fiyatı
+        public int UnitsInStock { get; set; }  //Stok Adedi
+
+    }
+}
